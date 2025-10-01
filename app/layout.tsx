0d37e1c8879e6a48b5b1d78b890739b3b367a3b1
minthura888 <minthura.joker@gmail.com>
2025-10-01 13:26:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
+// app/layout.tsx
 "use client";
 
 import Script from "next/script";
 import PixelRouteTracker from "./components/PixelRouteTracker";
 
+const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || "1865880404348903";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        {/* Meta Pixel Code */}
+        {/* Meta Pixel: init + FIRST LOAD PageView */}
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
-            fbq('track', 'PageView');
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView'); // first load ONLY
           `}
         </Script>
         <noscript>
@@ -27,13 +27,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             height="1"
             width="1"
             style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
+            src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
           />
         </noscript>
       </head>
       <body>
         {children}
-        <PixelRouteTracker /> {/* Tracks navigation */}
+        {/* Tracks client-side navigations; skips first render */}
+        <PixelRouteTracker />
       </body>
     </html>
   );
