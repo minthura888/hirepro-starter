1861d3f8f743dd27fdd31dd380e55dd8f51aab12
minthura888 <minthura.joker@gmail.com>
2025-10-01 19:14:38 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "HirePro Starter",
@@ -15,23 +16,33 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         {META_PIXEL_ID ? (
           <>
-            {/* Meta Pixel base code */}
-            <script
+            {/* Single, guarded Meta Pixel init */}
+            <Script
+              id="fb-pixel"
+              strategy="afterInteractive"
               dangerouslySetInnerHTML={{
                 __html: `
-                  !function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '${META_PIXEL_ID}');
-                  fbq('track', 'PageView');
+                  (function(){
+                    if (typeof window === 'undefined') return;
+                    if (window.__fbqInitialized) return;
+
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+
+                    try { fbq('init', '${META_PIXEL_ID}'); } catch(e) {}
+                    try { fbq('track', 'PageView'); } catch(e) {}
+                    window.__fbqInitialized = true;
+                  })();
                 `,
               }}
             />
+            {/* noscript image fallback is fine */}
             <noscript>
               <img
                 height="1"
