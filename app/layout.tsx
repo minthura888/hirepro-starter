9e3d04de196288c5958d69d59e5235fc519d0253
minthura888 <minthura.joker@gmail.com>
2025-10-01 11:35:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
-import PixelRouteTracker from "./components/PixelRouteTracker";
-import { Suspense } from "react";
-
-// Use env var if present, otherwise fall back to your ID
-const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || "1865880404348903";
 
 export const metadata: Metadata = {
   title: "HirePro Starter",
@@ -17,45 +12,37 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        {/* Meta Pixel base code: INIT ONLY (no PageView here) */}
-        {PIXEL_ID ? (
-          <Script
-            id="meta-pixel"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${PIXEL_ID}');
-              `,
-            }}
-          />
-        ) : null}
+        {/* Meta Pixel base code (hardcoded ID + fires PageView) */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1865880404348903');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
 
-        {/* Fire PageView on first load + each route change */}
-        <Suspense>
-          <PixelRouteTracker />
-        </Suspense>
-
         {/* noscript fallback */}
         <noscript>
-          {PIXEL_ID ? (
-            <img
-              height="1"
-              width="1"
-              style={{ display: "none" }}
-              src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
-            />
-          ) : null}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1865880404348903&ev=PageView&noscript=1"
+          />
         </noscript>
       </body>
     </html>
