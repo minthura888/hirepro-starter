54a4b08a3e293db1dca64d558585ad9cb8385db1
minthura888 <minthura.joker@gmail.com>
2025-10-01 13:19:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,39 @@
-// app/layout.tsx
-import type { Metadata } from "next";
+"use client";
+
 import Script from "next/script";
-import "./globals.css";
-import { Suspense } from "react";
 import PixelRouteTracker from "./components/PixelRouteTracker";
-
-const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || "1865880404348903";
-
-export const metadata: Metadata = {
-  title: "HirePro Starter",
-  description: "Landing page",
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        {/* Meta Pixel: INIT ONLY (no PageView here) */}
-        <Script
-          id="meta-pixel-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${PIXEL_ID}');
-            `,
-          }}
-        />
-      </head>
-      <body>
-        {children}
-
-        {/* PageView tracker (ensures exactly one hit per URL) */}
-        <Suspense>
-          <PixelRouteTracker />
-        </Suspense>
-
-        {/* noscript fallback */}
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
           />
         </noscript>
+      </head>
+      <body>
+        {children}
+        <PixelRouteTracker /> {/* Tracks navigation */}
       </body>
     </html>
   );
